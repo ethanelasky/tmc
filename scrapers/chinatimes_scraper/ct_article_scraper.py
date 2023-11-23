10e249f2b9e6f45f5296b55d13b1dbdfd416c8af
Ethan Elasky <elaskyethan@gmail.com>
2023-11-23 12:02:12 -0800

    Saving local changes

--- scrapers/chinatimes_scraper/ct_article_scraper.py
+++ scrapers/chinatimes_scraper/ct_article_scraper.py
@@ -34,7 +34,7 @@ async def scrape_text_from_page(urls, delay=5.0):
 
     assert len(sys.argv) == 3
 
-    df = pd.DataFrame(columns=['link', 'title', 'sections', 'time', 'article text'])
+    df = pd.DataFrame(columns=['link', 'title', 'sections', 'time', 'article text', 'author'])
 
     path_to_extension = "./uBlock"
     user_data_dir = "/tmp/test-user-data-dir"
@@ -64,25 +64,21 @@ async def scrape_text_from_page(urls, delay=5.0):
                 break
 
             source = await page.locator('div.source').all_inner_texts()
-
+            author = await page.locator('div.author').all_inner_texts()
             title = await page.locator('css=.article-title').inner_text()
-
-            times = await page.locator("css=time").get_attribute("datetime")
-            time = next(filter(None, times))
-
-
+            time = await page.locator("css=time").get_attribute("datetime")
             paragraphs = await page.locator("css=div.article-body > p").all_inner_texts()
             article_text = '\n'.join(paragraphs)
 
             # print(url, sections, title, time, article_text) #debugging
-            df.loc[len(df.index)] = [url, title, source, time, article_text]
+            df.loc[len(df.index)] = [url, title, source, time, article_text, author]
 
             await asyncio.sleep(delay)
 
             if i % 100 == 0 or i == len(urls) - 1:
                 df.to_csv(path_or_buf=sys.argv[2], mode='w')
                 print(f"Saved {len(df.index)} articles to file.")
-                df = pd.DataFrame(columns=['link', 'title', 'sections', 'time', 'article text'])
+                df = pd.DataFrame(columns=['link', 'title', 'sections', 'time', 'article text', 'author'])
 
         await browser.close()
     
@@ -90,8 +86,6 @@ async def scrape_text_from_page(urls, delay=5.0):
 # The delay in seconds between requests
 request_delay = 3.0 
 
-with open(sys.argv[1]) as f:
-    urls = f.read().split('\n')
-    urls = list(filter(None, urls))
+urls = pd.read_csv(sys.argv[1])['link']
 
 asyncio.run(scrape_text_from_page(urls, request_delay))
---
+++ scrapers/df_splitter.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import sys
+
+def split_dataset(filename):
+
+
+    if sys.argv[1][-3:] == ('txt'):
+        df = pd.read_csv(filename)
+        chunk_ranges = range(0, len(df.index), 100000) 
+        for i in range(len(chunk_ranges) - 1):
+            df.iloc[chunk_ranges[i]:chunk_ranges[i+1], :].to_csv(sys.argv[1][:-4] + "_" + str(i) + ".csv")
+
+split_dataset(sys.argv[1])
