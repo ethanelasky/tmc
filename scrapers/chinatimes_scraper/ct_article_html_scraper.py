1ae0182b3254bc6af99a399a814067502127daac
ethanelasky <elaskyethan@gmail.com>
2023-11-24 14:24:41 -0800

    Fixed html scraper for CT

--- scrapers/chinatimes_scraper/ct_article_html_scraper.py
+++ scrapers/chinatimes_scraper/ct_article_html_scraper.py
@@ -15,43 +15,35 @@ import sys
 
 async def scrape_html(urls, delay=5.0):
     """ 
-    Scrapes article contents from webpages given a list of URLs. The
-    contents of each article consists of its link, title, section, subsection,
-    date and time of publication, and the text of the article. 
-
-    After collecting 100 articles or upon reaching the end of the list of
-    urls, append the dataframe to a CSV file of links. Then clear the DataFrame
-    to avoid storing too much information in memory.
+    Scrapes article HTML from webpages given a list of URLs.
     
     Inputs: 
     - A list of URLs (as strings)
     - delay: minimum time between requests (to avoid IP bans / overthrottling)
 
     Outputs:
-    - A CSV file containing article contents named as specified in command
-        line arguments
+    - HTML files that each represent one article. 
     """
 
     path_to_extension = "./uBlock"
     user_data_dir = "/tmp/test-user-data-dir"
+    #ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+        
 
     async with async_playwright() as p:
         browser = await p.chromium.launch_persistent_context(
         user_data_dir,
         headless=False,
+        #user_agent=ua,
         args=[
-            "--headless=new",
+            #"--headless=new",
             f"--disable-extensions-except={path_to_extension}",
             f"--load-extension={path_to_extension}",
         ],
         )
 
-        ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-
-
         page = await browser.new_page()
         page.set_default_navigation_timeout(60000)
-        context = browser.new_context(user_agent=ua)
 
         for i in range(len(urls)):
             url = urls[i]
@@ -65,14 +57,12 @@ async def scrape_html(urls, delay=5.0):
                 break
 
             title = await page.locator('css=.article-title').inner_text()
-            contents = page.content()
-
+            contents = await page.content()
             with open(title + ".html", 'w') as f:
                 f.write(contents)
-                print("Wrote {title} to file.")
+                print(f"Wrote {title} to file.")
 
             await asyncio.sleep(delay)
-
 
         await browser.close()
     
@@ -80,8 +70,10 @@ async def scrape_html(urls, delay=5.0):
 # The delay in seconds between requests
 request_delay = 3.0 
 
-with open(sys.argv[1]) as f:
-    urls = f.read().split('\n')
-    urls = list(filter(None, urls))
+urls = pd.read_csv(sys.argv[1], index_col=0)['link'].values
+
+# with open(sys.argv[1]) as f:
+#     urls = f.read().split('\n')
+#     urls = list(filter(None, urls))
 
 asyncio.run(scrape_html(urls, request_delay))
