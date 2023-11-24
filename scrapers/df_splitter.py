934b3c90b7b3704902682481565cc1078ab2d273
ethanelasky <elaskyethan@gmail.com>
2023-11-24 14:25:57 -0800

    Fixed df_splitter

--- scrapers/df_splitter.py
+++ scrapers/df_splitter.py
@@ -5,7 +5,7 @@ def split_dataset(filename):
 
 
     if sys.argv[1][-3:] == ('txt'):
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename, names=["link", 'page'])
         chunk_ranges = range(0, len(df.index), 100000) 
         for i in range(len(chunk_ranges) - 1):
             df.iloc[chunk_ranges[i]:chunk_ranges[i+1], :].to_csv(sys.argv[1][:-4] + "_" + str(i) + ".csv")
