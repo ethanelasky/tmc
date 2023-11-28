88a9a734122a6bf933052427078ead0967415de6
ethanelasky <elaskyethan@gmail.com>
2023-11-28 12:09:21 -0800

    Rewrote LTN link scraper in scrapy

---
+++ scrapers/ltn_html_getter/ltn_html_getter/spiders/html_getter.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import scrapy
+
+
+class HTMLGetterSpider(scrapy.Spider):
+    name = "html_getter"
+    allowed_domains = ["ltn.com.tw"]
+
+    start_urls = pd.read_parquet("ltn_links.parquet")['link'].values.tolist()
+
+    def parse(self, response):
+        yield {'link': response.url, 'html': response.text, 'status': response.status}
